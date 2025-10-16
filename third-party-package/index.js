bc7b87c192818a39931bf8ef3cb10fbf334095b5
Christian Ekrem <christianekrem@gmail.com>
2025-10-16 14:11:29 +0200

    skitten poc break

--- third-party-package/index.js
+++ third-party-package/index.js
@@ -1,7 +1,12 @@
+const EvilObject = { ...Object };
 
+// må finne en mer sofistikert måte å hente prototype på, dette funker ikke helt
+EvilObject.prototype = Object.getPrototypeOf(Object);
+EvilObject.freeze = (a) => a;
+
+global.Object = EvilObject;
 
 export default function thirdPartyFunction(data) {
-  data.name.firstName = 'Christian';
-  data.name.lastName = 'Ekrem';
+  data.name.firstName = "Christian";
+  data.name.lastName = "Ekrem";
 }
-
